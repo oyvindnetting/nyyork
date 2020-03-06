40658632dbcc9b3b316f8b89ce32e589ae0bc7cf
Øyvind Dahl <oyvind@ibento.no>
2020-03-06 18:14:31 +0100

    Fixed header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,39 @@
 import React from 'react';
 
 import Logo from '../images/logo.png';
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import { Link } from 'gatsby';
 
 const Header = () => {
 
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark navbar-fixed-top">
-                <AniLink fade className="navbar-brand" to="/">
+                <Link fade className="navbar-brand" to="/">
                     <img src={Logo} alt="logo" />
-                </AniLink>
+                </Link>
            
                 <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i className="fas fa-bars fa-1x"></i></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item" >
-                            <AniLink fade to="/wear" className="nav-link" activeClassName="nav-active">
+                            <Link fade to="/wear" className="nav-link" activeClassName="nav-active">
                                 WEAR
-                            </AniLink>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <AniLink fade to="/jeans"  className="nav-link" activeClassName="nav-active">
+                            <Link fade to="/jeans"  className="nav-link" activeClassName="nav-active">
                                 JEANS
-                            </AniLink>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <AniLink fade to="/omoss"  className="nav-link" activeClassName="nav-active">
+                            <Link fade to="/omoss"  className="nav-link" activeClassName="nav-active">
                                 OM OSS
-                            </AniLink>
+                            </Link>
                         </li>
                         <li className="nav-item" >
-                            <AniLink fade to="/english"  className="nav-link" activeClassName="nav-active">
+                            <Link fade to="/english"  className="nav-link" activeClassName="nav-active">
                                 ENGLISH
-                            </AniLink>
+                            </Link>
                         </li>
                     </ul>
                 </div>
